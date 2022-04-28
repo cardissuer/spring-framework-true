f417802c29c7b6d583cfbe6639d0d0a7484b67e0
cardissuer <cardissuer@gmail.com>
2022-04-28 18:39:14 -0500

    Spring April 26

--- Spring-14-ORM-Queries/src/main/java/com/cydeo/boostrap/DataGenerator.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/boostrap/DataGenerator.java
@@ -1,5 +1,8 @@
 package com.cydeo.boostrap;
 
+import com.cydeo.repository.CourseRepository;
+import com.cydeo.repository.DepartmentRepository;
+import com.cydeo.repository.EmployeeRepository;
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,9 +11,15 @@ import org.springframework.stereotype.Component;
 public class DataGenerator implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
+    private final DepartmentRepository departmentRepository;
+    private final EmployeeRepository employeeRepository;
+    private final CourseRepository courseRepository;
 
-    public DataGenerator(RegionRepository regionRepository) {
+    public DataGenerator(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository, CourseRepository courseRepository) {
         this.regionRepository = regionRepository;
+        this.departmentRepository = departmentRepository;
+        this.employeeRepository = employeeRepository;
+        this.courseRepository = courseRepository;
     }
 
     @Override
@@ -25,6 +34,34 @@ public class DataGenerator implements CommandLineRunner {
         System.out.println("findTopByCountry:" + regionRepository.findTop2ByCountry("Canada"));
 
         System.out.println("-----------------REGION END-----------------");
+
+        System.out.println("-----------------DEPARTMENT START-----------------");
+
+        System.out.println("findByDepartment:" + departmentRepository.findByDepartment("Toys"));
+        System.out.println("findByDivisionIs:" + departmentRepository.findByDivisionIs("Outdoors"));
+        System.out.println("findDistinctTop3ByDivisionContains:" + departmentRepository.findDistinctTop3ByDivisionContains("Hea"));
+
+
+
+        System.out.println("-----------------DEPARTMENT END-----------------");
+
+        System.out.println("-----------------EMPLOYEE START-----------------");
+
+        System.out.println("getEmployeeDetail:" + employeeRepository.getEmployeeDetail());
+        System.out.println("getEmployeeSalary:" + employeeRepository.getEmployeeSalary());
+
+
+
+        System.out.println("-----------------EMPLOYEE  END-----------------");
+
+        System.out.println("-----------------COURSE START-----------------");
+
+        courseRepository.findByCategory("Spring").forEach(System.out::println);
+
+
+
+        System.out.println("-----------------COURSE  END-----------------");
+
 
     }
 }
